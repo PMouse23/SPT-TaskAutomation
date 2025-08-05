59cd1aeb3fb16dd7f859566877801d969e822db1
KnotScripts <Mouse2023Hub@gmail.com>
2025-08-05 10:25:03 +0200

    Version 0.6.0

--- Plugin.cs
+++ Plugin.cs
@@ -8,7 +8,7 @@ using TaskAutomation.Patches.Screens;
 
 namespace TaskAutomation
 {
-    [BepInPlugin("com.KnotScripts.TaskAutomation", "TaskAutomation", "0.5.1")]
+    [BepInPlugin("com.KnotScripts.TaskAutomation", "TaskAutomation", "0.6.0")]
     public class Plugin : BaseUnityPlugin
     {
         private ConfigEntry<bool> acceptBTROutOfRaid;
