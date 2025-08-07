d7a0976a817797f640890596b2318f26d90b2fbb
KnotScripts <Mouse2023Hub@gmail.com>
2025-08-07 23:32:45 +0200

    Fix for FIKA plugin: Added two more patches to set Globals.InRaid to false.

---
+++ Patches/Screens/SessionResultExitStatus_Show.cs
@@ -0,0 +1,31 @@
+﻿using EFT.UI.SessionEnd;
+using HarmonyLib;
+using SPT.Reflection.Patching;
+using System.Reflection;
+using TaskAutomation.Helpers;
+
+namespace TaskAutomation.Patches.Screens
+{
+    internal class SessionResultExitStatus_Show : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.FirstMethod(typeof(SessionResultExitStatus), this.IsTargetMethod);
+        }
+
+        [PatchPostfix]
+        private static void PatchPostfix()
+        {
+            Globals.InRaid = false;
+            if (Globals.Debug)
+                LogHelper.LogInfo($"inRaid={Globals.InRaid}");
+        }
+
+        private bool IsTargetMethod(MethodInfo method)
+        {
+            ParameterInfo[] parameters = method.GetParameters();
+            return method.Name == nameof(SessionResultExitStatus.Show)
+                && parameters.Length != 1;
+        }
+    }
+}
