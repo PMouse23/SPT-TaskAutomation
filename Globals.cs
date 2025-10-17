334bb1bf7960a30853c56ebb452dc9982d57c630
KnotScripts <Mouse2023Hub@gmail.com>
2025-10-17 23:51:13 +0200

    Added functionality to investigate coroutine. Ctrl + I

--- Globals.cs
+++ Globals.cs
@@ -19,6 +19,7 @@ internal static class Globals
     public static bool BlockTurnInCurrency = false;
     public static bool BlockTurnInWeapons = false;
     public static bool Debug = false;
+    public static KeyboardShortcut InvestigateKeys = new KeyboardShortcut(KeyCode.I, KeyCode.LeftControl);
     public static KeyboardShortcut ResetDeclinedHandoverItemConditionsKeys = new KeyboardShortcut(KeyCode.R, KeyCode.LeftControl);
     public static bool SkipElimination = false;
     public static bool SkipFindAndObtain = false;
