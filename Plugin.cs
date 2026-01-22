4541191aa61ef964dc618664d28fef2876b6f8a9
KnotScripts <Mouse2023Hub@gmail.com>
2026-01-22 23:25:04 +0100

    Version 1.2.3

--- Plugin.cs
+++ Plugin.cs
@@ -11,7 +11,7 @@ namespace TaskAutomation
     [BepInPlugin("com.KnotScripts.TaskAutomation", "TaskAutomation", VERSION)]
     public class Plugin : BaseUnityPlugin
     {
-        public const string VERSION = "1.2.2";
+        public const string VERSION = "1.2.3";
 
         private ConfigEntry<bool> acceptBTROutOfRaid;
         private ConfigEntry<bool> acceptLightKeeperOutOfRaid;
